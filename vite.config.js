e0ad07ef4ca1cb755a8647504dc0b48b30d22956
Cocobrei <brigettefayep.mabalot05@gmail.com>
2026-09-06 20:38:43 +0800

    test10

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/Activity_1_Mabalot/"
-=======
-  base: "/Activity_1_Mabalot/",
->>>>>>> 5f79c840652a7a1d04446d420dc2aef4e24cbb54
 })
